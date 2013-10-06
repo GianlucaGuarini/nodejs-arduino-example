371bfc701c8552dce58b63bbd42768841d6933e1
GianlucaGuarini <gianluca.guarini@gmail.com>
2013-10-06 22:24:21 +0200

    first commit

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,22 +8,22 @@ bool buttonClicked = false;
 void setup()
 {
   // set the bps
-	Serial.begin(9600);
+  Serial.begin(9600);
   // set the input pin
-	pinMode(pinNumber, INPUT);
+  pinMode(pinNumber, INPUT);
 }
  
 void loop()
 {
-	// read the button state
-	int tmpButtonState = digitalRead(pinNumber);
-	// if it has not been clicked yet and it's down we can send the message
-	if (tmpButtonState == HIGH && buttonClicked == false) {
-		Serial.print("ciao");
-		buttonClicked = true;
+  // read the button state
+  int tmpButtonState = digitalRead(pinNumber);
+  // if it has not been clicked yet and it's down we can send the message
+  if (tmpButtonState == HIGH && buttonClicked == false) {
+    Serial.print("ciao");
+    buttonClicked = true;
     delay(500); // wait 500 ms before sending the next message
-	} 
-	// if the button is not pressed anymore we reset the buttonClicked variable to false
-	if (tmpButtonState == LOW) 
-		buttonClicked = false;
+  } 
+  // if the button is not pressed anymore we reset the buttonClicked variable to false
+  if (tmpButtonState == LOW) 
+    buttonClicked = false;
 }
