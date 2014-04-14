baf84e874592662f84357374ba9a1d408b718a9e
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-04-14 23:42:17 +0200

    debug led enabled

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -11,6 +11,8 @@ void setup()
   Serial.begin(9600);
   // set the input pin
   pinMode(pinNumber, INPUT);
+  // initialize the LED pin as an output:
+  pinMode(led, OUTPUT);  
 }
  
 void loop()
@@ -20,9 +22,13 @@ void loop()
   // if it has not been clicked yet and it's down we can send the message
   if (tmpButtonState == HIGH && buttonClicked == false) {
     Serial.print("ciao");
+    digitalWrite(led, HIGH); 
     buttonClicked = true;
     delay(500); // wait 500 ms before sending the next message
-  } 
+  } else {
+    // just for debugging
+    digitalWrite(led, LOW); 
+  }
   // if the button is not pressed anymore we reset the buttonClicked variable to false
   if (tmpButtonState == LOW) 
     buttonClicked = false;
