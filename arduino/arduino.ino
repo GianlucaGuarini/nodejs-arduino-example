81213a31f48e6c2f2e3d891c6cdef75263405aa9
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-04-15 23:37:22 +0200

    stable release

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,28 +8,28 @@ bool buttonClicked = false;
 void setup()
 {
   // set the bps
-  Serial.begin(9600);
+  Serial.begin(57600);
   // set the input pin
   pinMode(pinNumber, INPUT);
   // initialize the LED pin as an output:
-  pinMode(led, OUTPUT);  
+  pinMode(led, OUTPUT);
 }
- 
+
 void loop()
 {
   // read the button state
   int tmpButtonState = digitalRead(pinNumber);
   // if it has not been clicked yet and it's down we can send the message
   if (tmpButtonState == HIGH && buttonClicked == false) {
-    Serial.print("ciao");
-    digitalWrite(led, HIGH); 
+    Serial.print("Hello World");
+    digitalWrite(led, HIGH);
+    delay(1000); // wait 1 s before sending the next message
     buttonClicked = true;
-    delay(500); // wait 500 ms before sending the next message
   } else {
     // just for debugging
-    digitalWrite(led, LOW); 
+    digitalWrite(led, LOW);
   }
   // if the button is not pressed anymore we reset the buttonClicked variable to false
-  if (tmpButtonState == LOW) 
+  if (tmpButtonState == LOW)
     buttonClicked = false;
 }
--- server/server.js
+++ server/server.js
@@ -1,28 +1,48 @@
 /**
- * 
+ *
  * Public variables
- * 
+ *
  */
 
 // load serialport
-var SerialPort = require('serialport').SerialPort,
-  // initialize serialport using the /dev/ttyACM0 serial port
+var app = require('http').createServer(handler),
+  io = require('socket.io').listen(app),
+  fs = require('fs'),
+  SerialPort = require('serialport').SerialPort,
+  // initialize serialport using the /dev/cu.usbmodem1411 serial port
   // remember to change this string if your arduino is using a different serial port
-  sp = new SerialPort('/dev/ttyACM0', {
-    baudrate: 9600
+  sp = new SerialPort('/dev/cu.usbmodem1411', {
+    baudrate: 57600
   }),
   // this var will contain the output string coming from arduino
   outputStr = '',
   // timer used to output correctly all the messages coming from arduino
   outputTimer;
+
+// creating the server ( localhost:8000 )
+app.listen(8000);
+
+// on server started we can load our client.html page
+function handler(req, res) {
+  fs.readFile('client/client.html', function(err, data) {
+    if (err) {
+      console.log(err);
+      res.writeHead(500);
+      return res.end('Error loading client.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
 /**
- * 
+ *
  * This function is used as proxy to print the arduino messages into the nodejs console
  * @param  { Buffer } buffer: buffer data sent via serialport
- * 
+ *
  */
 
-var printOutput = function (buffer) {
+var printOutput = function(buffer, socket) {
   // concatenating the string buffers sent via usb port
   outputStr += buffer.toString();
 
@@ -31,17 +51,22 @@ var printOutput = function (buffer) {
 
   // wait 500 ms and then output the message
   outputTimer = setTimeout(function() {
+    // log the message into the terminal
     console.log(outputStr);
+    // send the message to the client
+    socket.volatile.emit('notification', outputStr);
     // reset the output string to an empty value
     outputStr = '';
   }, 500);
 };
 
-// listen all the serial port messages sent from arduino and passing them to the proxy function printOutput
-sp.on('data', function(data) {
-  printOutput(data);
+// creating a new websocket
+io.sockets.on('connection', function(socket) {
+  // listen all the serial port messages sent from arduino and passing them to the proxy function printOutput
+  sp.on('data', function(data) {
+    printOutput(data, socket);
+  });
 });
-
 
 // just some debug listeners
 sp.on('close', function(err) {
